811f14be5861c86323843e1c3e1f61cfe09c49e4
Aditya Gupta <97134306+aaggupta07@users.noreply.github.com>
2025-07-28 12:16:19 -0500

    Create Etch-a-Sketch project, implement grid
    Add new project to website/repo, allow grid to resize with window size

---
+++ etch-a-sketch/script.js
@@ -0,0 +1,38 @@
+let gridSize = 16;
+
+// function debounce(fn, delay) {
+//     let timeoutID;
+
+//     return function(...args) {
+//         clearTimeout(timeoutID);
+//         timeoutID = setTimeout(() => fn(...args), delay);
+//     }
+// }
+
+function createBoard() {
+    const container = document.querySelector(".container");
+    const curElements = container.querySelectorAll(".row");
+    curElements.forEach((element) => element.remove());
+
+    for(let i = 0; i < gridSize; ++i) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+
+        const height = Math.floor(container.clientWidth / gridSize);
+        console.log("Called! Width: " + container.clientWidth + " Height: " + height);
+        row.style.height = `${height}px`;
+
+        for(let j = 0; j < gridSize; ++j) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+        }
+
+        container.appendChild(row);
+    }
+}
+
+createBoard(gridSize);
+
+addEventListener("resize", createBoard);
+
